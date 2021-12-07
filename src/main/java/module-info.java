5504d5223381d89c48e0f9090faa9f19d0a212d5
Oswin Nightingale <NightingaleOswin@gmail.com>
2021-12-07 21:57:55 +0100

    General Fixes
    Rewrote App.java, AppController.java and pom.xml
    created dependency-reduced-pom.xml
    
    TODO: Login.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module vut.cz.bpcbdsproject3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.slf4j;
 
 
     opens vut.cz.bpcbdsproject3 to javafx.fxml;
--- src/main/java/vut/cz/bpcbdsproject3/AppController.java
+++ src/main/java/vut/cz/bpcbdsproject3/AppController.java
@@ -1,16 +1,60 @@
 package vut.cz.bpcbdsproject3;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AppController
 {
-    @FXML
-    private Label welcomeText;
+    private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @FXML
-    protected void onHelloButtonClick()
+    public Label usernameLabel;
+    @FXML
+    public Label passwordLabel;
+    @FXML
+    private TextField usernameText;
+    @FXML
+    private PasswordField passwordText;
+    @FXML
+    private Button signInButton;
+
+    // private PersonRepository personRepository;
+    // private AuthService authService;
+
+    public AppController()
     {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    }
+
+    @FXML
+    private void init()
+    {
+        usernameText.setOnKeyPressed(event ->
+        {
+            if (event.getCode() == KeyCode.ENTER)
+                {
+                    signIn();
+                }
+        });
+        passwordText.setOnKeyPressed(event ->
+        {
+            if (event.getCode() == KeyCode.ENTER)
+                {
+                    signIn();
+                }
+        });
+        logger.info("AppController started");
+    }
+
+    private void signIn()
+    {
+        String username = usernameText.getText();
+        String password = passwordText.getText();
     }
 }
